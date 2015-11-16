e0c0b499f5e55944fa2621c874ff90e5800e9ee3
KamataRyo <mugil.cephalus+github@gmail.com>
2015-11-16 17:13:23 +0900

    enable bootstrap

--- functions.php
+++ functions.php
@@ -114,11 +114,17 @@ add_action( 'widgets_init', 'kamome_note_widgets_init' );
  * Enqueue scripts and styles.
  */
 function kamome_note_scripts() {
-	wp_enqueue_style( 'kamome-note-style', get_stylesheet_uri() );
+	$lib_uri = get_template_directory_uri() . '/js/lib' ;
+
+	//defaults
+	wp_enqueue_style( 'kamome-note-style', get_stylesheet_uri(), 'bootstrap' );
 
 	wp_enqueue_script( 'kamome-note-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'kamome-note-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	// additional
+	wp_enqueue_style( 'bootstrap', $lib_uri . '/bootstrap/dist/css/bootstrap.min.css', 'jquery'  );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
