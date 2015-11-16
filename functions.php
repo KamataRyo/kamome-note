0bbe7184d4a1dbd061ae1a7e77d2c260b97b665b
KamataRyo <mugil.cephalus+github@gmail.com>
2015-11-16 18:09:38 +0900

    boostrtap rtl installed

--- functions.php
+++ functions.php
@@ -125,7 +125,9 @@ function kamome_note_scripts() {
 	wp_enqueue_script( 'kamome-note-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	// additional
-	wp_enqueue_style( 'bootstrap', $lib_uri . '/bootstrap/dist/css/bootstrap.min.css', 'jquery'  );
+	wp_enqueue_style( 'bootstrap', $lib_uri . '/bootstrap/dist/css/bootstrap.min.css', array( 'jquery' )  );
+	#wp_enqueue_style( 'bootstrap-rtl', $lib_uri . '/bootstrap/dist/css/bootstrap-rtl.min.css', array( 'jquery', 'bootstrap')  );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
